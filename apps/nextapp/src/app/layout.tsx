29d0b6239ae359a332d96b4f8daadae729f01dd5
RChursin <ganstacoder@gmail.com>
2024-11-05 17:35:17 -0800

    Added Font

--- apps/nextapp/src/app/layout.tsx
+++ apps/nextapp/src/app/layout.tsx
@@ -1,14 +1,15 @@
 import { RootLayout } from '../components/layouts/root-layout';
 import { ThemeProvider } from '../components/themes/theme-provider';
-import { Inter } from 'next/font/google';
+// import { Inter } from 'next/font/google';
 import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] });
+// const inter = Inter({ subsets: ['latin'] });
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${inter.className} dark`}>
+      {/* <body className={`${inter.className} dark`}></body> */}
+      <body className="dark">
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
           <RootLayout>{children}</RootLayout>
         </ThemeProvider>
